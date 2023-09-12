6c2cd5606a86184fc535c8e90d414c201a023765
Josh Karta <josh.kartadiwirja@kabinetpresident.gov.sr>
2023-09-12 15:00:32 -0300

    temp

--- views/countries/index.php
+++ views/countries/index.php
@@ -12,6 +12,7 @@ use yii\grid\GridView;
 
 $this->title = 'Countries';
 $this->params['breadcrumbs'][] = $this->title;
+// $this->registerFile('@web/js/main.js',['position'=> \yii\web\View::POS_END]);
 ?>
 <div class="countries-index">
 
@@ -41,4 +42,22 @@ $this->params['breadcrumbs'][] = $this->title;
     ]); ?>
 
 
+    <button id="liveToastBtn">TestButton</button>
+            <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+            <img src="..." class="rounded me-2" alt="...">
+            <strong class="me-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+            Hello, world! This is a toast message.
+            </div>
+        </div>
+        </div>
 </div>
+
+<script>
+    Tester()
+</script>
---
+++ web/js/main.js
@@ -0,0 +1,12 @@
+function Tester() {
+  const toastTrigger = document.getElementById("liveToastBtn");
+  const toastLiveExample = document.getElementById("liveToast");
+
+  if (toastTrigger) {
+    const toastBootstrap =
+      bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastTrigger.addEventListener("click", () => {
+      toastBootstrap.show();
+    });
+  }
+}
